c5870e8f8827ba2557be8ec0e39c7365e50c1782
Thomas Papke <web@thopap.de>
2023-11-08 20:38:31 +0100

    Disable log spam in case of validation failures.

--- src/main/java/org/openehealth/app/xdstofhir/registry/XdsRouteBuilder.java
+++ src/main/java/org/openehealth/app/xdstofhir/registry/XdsRouteBuilder.java
@@ -5,10 +5,12 @@ import static org.openehealth.ipf.platform.camel.ihe.xds.XdsCamelValidators.iti1
 import static org.openehealth.ipf.platform.camel.ihe.xds.XdsCamelValidators.iti42RequestValidator;
 import static org.openehealth.ipf.platform.camel.ihe.xds.XdsCamelValidators.iti42ResponseValidator;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.openehealth.app.xdstofhir.registry.patientfeed.Iti8Service;
 import org.openehealth.app.xdstofhir.registry.query.Iti18Service;
 import org.openehealth.app.xdstofhir.registry.register.Iti42Service;
+import org.openehealth.ipf.commons.ihe.xds.core.validate.XDSMetaDataException;
 import org.openehealth.ipf.platform.camel.ihe.mllp.PixPdqCamelValidators;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,12 @@ public class XdsRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        // Relax logging from Error to warn without stacktrace for validation failures
+        onException(XDSMetaDataException.class)
+            .handled(false)
+            .log(LoggingLevel.WARN, "Reject invalid request due to: ${exception.message}")
+            .logExhausted(false);
+
         from("{{xds.endpoint.iti18}}")
             .routeId("iti18Route")
             .process(iti18RequestValidator())
