3508c428a09b4f9244f17e3c2c9053b5e49eb48c
Thomas Papke <web@thopap.de>
2024-04-01 16:59:00 +0200

    #2 Add support for Remove Metadata (ITI-62)
    * Fix validation handling for RPLC associations

--- src/main/java/org/openehealth/app/xdstofhir/registry/remove/RemoveDocumentsProcessor.java
+++ src/main/java/org/openehealth/app/xdstofhir/registry/remove/RemoveDocumentsProcessor.java
@@ -48,9 +48,9 @@ public class RemoveDocumentsProcessor implements Iti62Service {
 
         var docBundleResult = client.search().forResource(DocumentReference.class)
                 .withProfile(MappingSupport.MHD_COMPREHENSIVE_PROFILE)
-                .include(DocumentReference.INCLUDE_RELATESTO)
+                .include(DocumentReference.INCLUDE_RELATED)
                 .revInclude(ListResource.INCLUDE_ITEM)
-                .revInclude(DocumentReference.INCLUDE_RELATED)
+                .revInclude(DocumentReference.INCLUDE_RELATESTO)
                 .where(DocumentReference.IDENTIFIER.exactly().systemAndValues(URI_URN,uuidsToDelete))
                 .returnBundle(Bundle.class)
                 .execute();
@@ -58,32 +58,17 @@ public class RemoveDocumentsProcessor implements Iti62Service {
         // collect all List Resources (SubmissionSet, Folder) without duplicates (hapi domain objects do not implement equal / hashcode)
         var uniqueResults = new TreeSet<ListResource>((a, b) -> Comparator.comparing(IAnyResource::getId).compare(a,  b));
 
-        var docResult = new PagingFhirResultIterator<DocumentReference>(docBundleResult, DocumentReference.class, client);
+        var docResult = BundleUtil.toListOfResourcesOfType(client.getFhirContext(), docBundleResult, DocumentReference.class);
         uniqueResults.addAll(BundleUtil.toListOfResourcesOfType(client.getFhirContext(), docBundleResult, ListResource.class));
-        docResult.forEachRemaining(doc -> {
+
+        docResult.forEach(doc -> {
             processAssociations(doc, doc.getRelatesTo(), uuidsToDelete, builder);
             if (addToDeleteTransaction(doc, uuidsToDelete, builder)) {
                 validateNoReferencesExists(doc, docResult, unmodifiedUuidsToDelete, errorInfo);
             }
         });
 
-        var reverseSearchCriteria = Collections.singletonMap("item:identifier", Collections.singletonList(
-                uuidsToDelete.stream().map(MappingSupport::toUrnCoded).map(urnCoded -> URI_URN + "|" + urnCoded).collect(Collectors.joining(","))));
-        var referenceQuery = client.search().forResource(ListResource.class)
-                .whereMap(reverseSearchCriteria)
-                .revInclude(ListResource.INCLUDE_ITEM)
-                .returnBundle(Bundle.class);
-
-        var listQuery = client.search().forResource(ListResource.class)
-                .where(ListResource.IDENTIFIER.exactly().systemAndValues(URI_URN,uuidsToDelete))
-                .revInclude(ListResource.INCLUDE_ITEM)
-                .returnBundle(Bundle.class);
-
-        new PagingFhirResultIterator<ListResource>(referenceQuery.execute(), ListResource.class, client)
-                .forEachRemaining(uniqueResults::add);
-
-        new PagingFhirResultIterator<ListResource>(listQuery.execute(), ListResource.class, client)
-                .forEachRemaining(uniqueResults::add);
+        uniqueResults.addAll(fetchFolderAndSubmissionSets(uuidsToDelete));
 
         uniqueResults.forEach(ref -> {
             processAssociations(ref, ref.getEntry(), uuidsToDelete, builder);
@@ -117,22 +102,47 @@ public class RemoveDocumentsProcessor implements Iti62Service {
         return response;
     }
 
+    private List<ListResource> fetchFolderAndSubmissionSets(List<String> uuidsToDelete) {
+        var result = new ArrayList<ListResource>();
+        var reverseSearchCriteria = Collections.singletonMap("item:identifier", Collections.singletonList(
+                uuidsToDelete.stream().map(MappingSupport::toUrnCoded).map(urnCoded -> URI_URN + "|" + urnCoded).collect(Collectors.joining(","))));
+        var referenceQuery = client.search().forResource(ListResource.class)
+                .whereMap(reverseSearchCriteria)
+                .revInclude(ListResource.INCLUDE_ITEM)
+                .returnBundle(Bundle.class);
+
+        var listQuery = client.search().forResource(ListResource.class)
+                .where(ListResource.IDENTIFIER.exactly().systemAndValues(URI_URN,uuidsToDelete))
+                .revInclude(ListResource.INCLUDE_ITEM)
+                .returnBundle(Bundle.class);
+
+        new PagingFhirResultIterator<ListResource>(referenceQuery.execute(), ListResource.class, client)
+                .forEachRemaining(result::add);
+
+        new PagingFhirResultIterator<ListResource>(listQuery.execute(), ListResource.class, client)
+                .forEachRemaining(result::add);
+        return result;
+    }
+
     private void validateNoReferencesExists(DocumentReference doc,
-            PagingFhirResultIterator<DocumentReference> docResult, ArrayList<String> uuidsToDelete, List<ErrorInfo> errorInfo) {
+            List<DocumentReference> docResult, List<String> uuidsToDelete, List<ErrorInfo> errorInfo) {
         var listOfRelations = new ArrayList<DocumentReferenceRelatesToComponent>();
-        docResult.forEachRemaining(aDoc -> {
+        docResult.forEach(aDoc -> {
             var entryUuid = StoredQueryMapper.entryUuidFrom(aDoc);
             if (!uuidsToDelete.contains(entryUuid) && aDoc != doc)
                 listOfRelations.addAll(aDoc.getRelatesTo());
         });
-        doc.getRelatesTo().stream().filter(docRel -> docRel.getCode().equals(DocumentRelationshipType.REPLACES)).forEach(relDoc -> {
+        doc.getRelatesTo().stream().filter(docRel -> DocumentRelationshipType.REPLACES.equals(docRel.getCode())).forEach(relDoc -> {
             var entryUuid = StoredQueryMapper.entryUuidFrom(relDoc.getTarget().getResource());
             if (!uuidsToDelete.contains(entryUuid)){
                 errorInfo.add(new ErrorInfo(ErrorCode.REFERENCE_EXISTS_EXCEPTION, "Some references still exists to " + entryUuid,
                         Severity.ERROR, null, null));
             }
         });
-        listOfRelations.stream().map(DocumentReferenceRelatesToComponent::getTarget).filter(Objects::nonNull)
+        listOfRelations.stream()
+                .filter(rel -> !uuidsToDelete.contains(rel.getId()))
+                .map(DocumentReferenceRelatesToComponent::getTarget)
+                .filter(Objects::nonNull)
                 .map(Reference::getResource).filter(DocumentReference.class::isInstance)
                 .map(DocumentReference.class::cast)
                 .filter(docRef -> docRef.equalsDeep(doc))
